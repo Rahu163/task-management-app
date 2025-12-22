4b29da2309e34295e4a0876d999098b3c574fc49
Rahul Veeramalla <rahulgoud11181@gmail.com>
2025-12-22 10:19:02 +0530

    commit

--- backend/routes/tasks.js
+++ backend/routes/tasks.js
@@ -381,11 +381,7 @@ router.put("/:id", async (req, res) => {
     // Find task that user has access to
     let task = await Task.findOne({
       _id: req.params.id,
-      $or: [
-        { createdBy: req.user._id },
-        { assignee: req.user._id },
-        { assignee: "all" },
-      ],
+      $or: [{ createdBy: req.user._id }, { assignee: req.user._id }],
     });
 
     if (!task) {
@@ -432,9 +428,6 @@ router.put("/:id", async (req, res) => {
       // Notify old assignee if they were removed or changed
       if (
         oldAssignee &&
-        oldAssignee !== "all" &&
-        task.assignee &&
-        task.assignee !== "all" &&
         oldAssignee.toString() !== task.assignee._id.toString() &&
         oldAssignee.toString() !== task.createdBy._id.toString()
       ) {
